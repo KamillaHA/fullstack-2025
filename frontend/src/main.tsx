89fc52c1c3a56aa9b0b46218de2a08a1bbac63e0
Kamilla Huhnke <kamillahuhnke@me.com>
2025-09-08 11:56:58 +0200

    hentet apikey og fået det til at virke

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
+import theme from "./theme";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
